aded4d695a96e92d3fc287bde6a8668fb34fe66a
Nick Miyake <nmiyake@palantir.com>
2018-11-30 11:47:49 -0800

    Fix content of warning output

--- wlog/logger_provider_warnonce.go
+++ wlog/logger_provider_warnonce.go
@@ -38,7 +38,7 @@ func (l *warnOnceLogger) SetLevel(level LogLevel)           { l.once.Do(l.printW
 
 func (l *warnOnceLogger) printWarning() {
 	_, _ = fmt.Fprintln(l.w, `[WARNING] Logging operation that uses the default logger provider was performed without specifying a logger provider implementation. `+
-		`To see logger output, set the global tracer implementation using wlog.SetDefaultLoggerProvider or by importing an implementation. `+
+		`To see logger output, set the global logger provider implementation using wlog.SetDefaultLoggerProvider or by importing an implementation. `+
 		`This warning can be disabled by setting the global logger provider to be the noop logger provider using wlog.SetDefaultLoggerProvider(wlog.NewNoopLoggerProvider()).`)
 }
 
