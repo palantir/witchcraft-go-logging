32419c76b557557553287e14948016a7c9e13b15
Nick Miyake <nmiyake@palantir.com>
2018-12-11 13:46:27 -0800

    Generate logging definitions using Conjure
    * Add godel-conjure-plugin
    * Add logging API Conjure IR definition
    * Generate Conjure
    * Remove internal conjuretype package

--- wlog/diaglog/diag1log/diag1logtests/tests.go
+++ wlog/diaglog/diag1log/diag1logtests/tests.go
@@ -20,10 +20,10 @@ import (
 	"io"
 	"testing"
 
+	"github.com/palantir/conjure-go/conjure/types/conjuretype"
 	"github.com/palantir/pkg/objmatcher"
 	"github.com/palantir/pkg/safejson"
 	"github.com/palantir/witchcraft-go-logging/conjure/sls/spec/logging"
-	"github.com/palantir/witchcraft-go-logging/internal/conjuretype"
 	"github.com/palantir/witchcraft-go-logging/wlog/diaglog/diag1log"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
