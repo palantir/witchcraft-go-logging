380e785cbbf890cf0bb9cf15e52b11521d583d34
Kevin Simons <ksimons@palantir.com>
2019-05-02 10:16:03 -0700

    Fix for single impl

--- wlog-zap/internal/marshalers/wtracing_spanmodel.go
+++ wlog-zap/internal/marshalers/wtracing_spanmodel.go
@@ -35,7 +35,7 @@ func marshalWTracingSpanModel(key string, val interface{}) zapcore.Field {
 			enc.AddString(trc1log.SpanParentIDKey, string(*parentID))
 		}
 		enc.AddInt64(trc1log.SpanTimestampKey, span.Timestamp.Round(time.Microsecond).UnixNano()/1e3)
-		enc.AddInt64(trc1log.SpanDurationKey, int64(span.Duration.Round(time.Microsecond)))
+		enc.AddInt64(trc1log.SpanDurationKey, roundDownDuration(&span))
 		if kind := span.Kind; kind != "" {
 			// if kind is non-empty, manually create v1-style annotations
 			switch kind {
@@ -51,6 +51,10 @@ func marshalWTracingSpanModel(key string, val interface{}) zapcore.Field {
 		}
 		return nil
 	}))
+}
+
+func roundDownDuration(span *wtracing.SpanModel) int64 {
+	return int64(span.Duration.Round(time.Microsecond) / 1e3)
 }
 
 func encodeSpanModelAnnotations(enc zapcore.ObjectEncoder, startVal, endVal string, span wtracing.SpanModel) error {
