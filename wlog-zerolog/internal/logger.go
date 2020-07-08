cb2129f5f0c04ce473c257a4748f6024a14240c7
Sean Moorman <smoorman@palantir.com>
2020-07-08 13:59:17 -0400

    manually track output log levels and always use zerolog.Log in order to avoid printing zerolog info level. All levels for zerolog are printed at the beginning of the message and use a lowercase enum. This lowercase is inconsistent with sls defined uppercase enums. Each field key should only appear once so we must print the correctly capitalized level names at the time that we apply params

--- wlog-zerolog/internal/logger.go
+++ wlog-zerolog/internal/logger.go
@@ -25,11 +25,25 @@ import (
 )
 
 type zeroLogEntry struct {
-	evt *zerolog.Event
+	evt  *zerolog.Event
+	keys map[string]int
 	wlog.MapValueEntries
 }
 
+func (e *zeroLogEntry) keyExists(key string) bool {
+	if i, exists := e.keys[key]; exists {
+		e.keys[key] = i + 1
+		return true
+	} else {
+		e.keys[key] = 1
+		return false
+	}
+}
+
 func (e *zeroLogEntry) StringValue(key, value string) {
+	if e.keyExists(key) {
+		return
+	}
 	e.evt = e.evt.Str(key, value)
 }
 
@@ -41,19 +55,31 @@ func (e *zeroLogEntry) OptionalStringValue(key, value string) {
 
 func (e *zeroLogEntry) StringListValue(k string, v []string) {
 	if len(v) > 0 {
+		if e.keyExists(k) {
+			return
+		}
 		e.evt.Strs(k, v)
 	}
 }
 
 func (e *zeroLogEntry) SafeLongValue(key string, value int64) {
+	if e.keyExists(key) {
+		return
+	}
 	e.evt = e.evt.Int64(key, value)
 }
 
 func (e *zeroLogEntry) IntValue(key string, value int32) {
+	if e.keyExists(key) {
+		return
+	}
 	e.evt = e.evt.Int32(key, value)
 }
 
 func (e *zeroLogEntry) ObjectValue(k string, v interface{}, marshalerType reflect.Type) {
+	if e.keyExists(k) {
+		return
+	}
 	ok := marshalers.EncodeType(e.evt, marshalerType, k, v)
 	if !ok {
 		e.evt.Interface(k, v)
@@ -85,43 +111,82 @@ func (e *zeroLogEntry) Evt() *zerolog.Event {
 
 type zeroLogger struct {
 	logger zerolog.Logger
+	level  zerolog.Level
+}
+
+func (l *zeroLogger) should(level zerolog.Level) bool {
+	if level < l.level {
+		return false
+	}
+	return true
 }
 
 func (l *zeroLogger) Log(params ...wlog.Param) {
+	if !l.should(zerolog.NoLevel) {
+		return
+	}
 	logOutput(l.logger.Log, "", "", params)
 }
 
 func (l *zeroLogger) Debug(msg string, params ...wlog.Param) {
-	logOutput(l.logger.Debug, msg, svc1log.LevelDebugValue, params)
+	if !l.should(zerolog.DebugLevel) {
+		return
+	}
+	logOutput(l.logger.Log, msg, svc1log.LevelDebugValue, params)
 }
 
 func (l *zeroLogger) Info(msg string, params ...wlog.Param) {
-	logOutput(l.logger.Info, msg, svc1log.LevelInfoValue, params)
+	if !l.should(zerolog.InfoLevel) {
+		return
+	}
+	logOutput(l.logger.Log, msg, svc1log.LevelInfoValue, params)
 }
 
 func (l *zeroLogger) Warn(msg string, params ...wlog.Param) {
-	logOutput(l.logger.Warn, msg, svc1log.LevelWarnValue, params)
+	if !l.should(zerolog.WarnLevel) {
+		return
+	}
+	logOutput(l.logger.Log, msg, svc1log.LevelWarnValue, params)
 }
 
 func (l *zeroLogger) Error(msg string, params ...wlog.Param) {
-	logOutput(l.logger.Error, msg, svc1log.LevelErrorValue, params)
+	if !l.should(zerolog.ErrorLevel) {
+		return
+	}
+	logOutput(l.logger.Log, msg, svc1log.LevelErrorValue, params)
 }
 
 func (l *zeroLogger) SetLevel(level wlog.LogLevel) {
+	l.level = toZeroLevel(level)
 	l.logger = l.logger.Level(toZeroLevel(level))
 }
 
+func reverseParams(params []wlog.Param) []wlog.Param {
+	for i, j := 0, len(params)-1; i < j; i, j = i+1, j-1 {
+		params[i], params[j] = params[j], params[i]
+	}
+	return params
+}
+
 func logOutput(newEvt func() *zerolog.Event, msg, levelVal string, params []wlog.Param) {
+	//TODO(smoorman): to remove duplicate level entries
+	// either:
+	// 	make newEvt produce a zerolog event without level printed already
 	entry := &zeroLogEntry{
-		evt: newEvt(),
+		evt:  newEvt(),
+		keys: make(map[string]int),
 	}
 	if !entry.evt.Enabled() {
 		return
 	}
 	entry.evt = entry.evt.Str(wlog.TimeKey, time.Now().Format(time.RFC3339Nano))
+	//TODO(smoorman):
+	// or:
+	//	remove these 3 lines
 	if levelVal != "" {
 		entry.evt = entry.evt.Str(svc1log.LevelKey, levelVal)
 	}
+	params = reverseParams(params)
 	wlog.ApplyParams(entry, params)
 	entry.Evt().Msg(msg)
 }
