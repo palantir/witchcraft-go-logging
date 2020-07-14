11af9938be18fbced562d1c1a5dd6a7da434dee7
James Ross <jross6007@gmail.com>
2020-07-14 09:30:27 -0700

    Add diag1log.WithLogger and diag1log.FromContext methods. (#96)
    * Add diag1log.WithLogger and diag1log.FromContext methods.
    
    * Add changelog
    
    * Remove unused testDiagLogger type
    
    Co-authored-by: James Ross <jross@palantir.com>

---
+++ wlog/diaglog/diag1log/context_test.go
@@ -0,0 +1,31 @@
+// Copyright (c) 2020 Palantir Technologies. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package diag1log_test
+
+import (
+	"context"
+	"io/ioutil"
+	"testing"
+
+	"github.com/palantir/witchcraft-go-logging/wlog/diaglog/diag1log"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWithLoggerFromContextRoundTrip(t *testing.T) {
+	expected := diag1log.New(ioutil.Discard)
+	ctx := diag1log.WithLogger(context.Background(), expected)
+	got := diag1log.FromContext(ctx)
+	assert.Equal(t, expected, got)
+}
