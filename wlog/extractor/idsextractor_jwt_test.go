f3d751ced15a73721053cdb2162e7a5a602fa6be
Oleksandr Kushchenko <gearok@gmail.com>
2025-10-07 17:41:40 +0100

    improvement: Extract JWT info from auth header on WebSocket requests (#471)

--- wlog/extractor/idsextractor_jwt_test.go
+++ wlog/extractor/idsextractor_jwt_test.go
@@ -15,13 +15,88 @@
 package extractor
 
 import (
+	"net/http"
+	"net/textproto"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+)
+
+const (
+	testJWTJohn = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwic2lkIjoiSm9obiBEb2UiLCJqdGkiOiIzNTBhY2MwYS0zNmU2LTQ3MWItOTUxOC1lODBiY2VlYjFmYzQiLCJvcmciOiJhNTY0OWJhNS04MDAyLTRhYzAtYTZkZi1hNTE3ZThlMDFjMTEiLCJpYXQiOjE3NTk4NDgwMzQsImV4cCI6MTc1OTg1MTYzNH0.hL32VOaNMzKxggOtQ_j4Yp4WxfInub3Sqt2VypWeHdY"
+	testJWTJane = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwic2lkIjoiSmFuZSBEb2UiLCJqdGkiOiIzNTBhY2MwYS0zNmU2LTQ3MWItOTUxOC1lODBiY2VlYjFmYzQiLCJvcmciOiJhNTY0OWJhNS04MDAyLTRhYzAtYTZkZi1hNTE3ZThlMDFjMTEiLCJpYXQiOjE3NTk4NDgwMzQsImV4cCI6MTc1OTg1MTYzNH0.vkI_jtLu53gak3doWhMNjsFmHL5gymvO10pfZN301YQ"
 )
 
 func Test_uuidFromBase64StdEncodedString(t *testing.T) {
 	in := "vp9kXVLgSem6MdsyknYV2w=="
 	expected := "be9f645d-52e0-49e9-ba31-db32927615db"
 	assert.Equal(t, expected, uuidFromBase64StdEncodedString(in))
+}
+
+func Test_jwtRequestIDsExtractor_ExtractIDs(t *testing.T) {
+	tests := []struct {
+		name string
+		req  *http.Request
+		want map[string]string
+	}{
+		{
+			name: "empty",
+			req:  &http.Request{},
+			want: map[string]string{
+				UIDKey:     "",
+				SIDKey:     "",
+				TokenIDKey: "",
+				OrgIDKey:   "",
+			},
+		},
+		{
+			name: "authorization header",
+			req: &http.Request{
+				Header: http.Header{
+					textproto.CanonicalMIMEHeaderKey("Authorization"): {"Bearer " + testJWTJohn},
+				},
+			},
+			want: map[string]string{
+				UIDKey:     "1234567890",
+				SIDKey:     "John Doe",
+				TokenIDKey: "350acc0a-36e6-471b-9518-e80bceeb1fc4",
+				OrgIDKey:   "a5649ba5-8002-4ac0-a6df-a517e8e01c11",
+			},
+		},
+		{
+			name: "websocket protocols header",
+			req: &http.Request{
+				Header: http.Header{
+					textproto.CanonicalMIMEHeaderKey("Sec-WebSocket-Protocol"): {"Bearer-" + testJWTJohn},
+				},
+			},
+			want: map[string]string{
+				UIDKey:     "1234567890",
+				SIDKey:     "John Doe",
+				TokenIDKey: "350acc0a-36e6-471b-9518-e80bceeb1fc4",
+				OrgIDKey:   "a5649ba5-8002-4ac0-a6df-a517e8e01c11",
+			},
+		},
+		{
+			name: "websocket protocols and authorization header",
+			req: &http.Request{
+				Header: http.Header{
+					textproto.CanonicalMIMEHeaderKey("Sec-WebSocket-Protocol"): {"Bearer-" + testJWTJohn},
+					textproto.CanonicalMIMEHeaderKey("Authorization"):          {"Bearer " + testJWTJane},
+				},
+			},
+			want: map[string]string{
+				UIDKey:     "1234567890",
+				SIDKey:     "Jane Doe",
+				TokenIDKey: "350acc0a-36e6-471b-9518-e80bceeb1fc4",
+				OrgIDKey:   "a5649ba5-8002-4ac0-a6df-a517e8e01c11",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := &jwtRequestIDsExtractor{}
+			assert.Equal(t, tt.want, e.ExtractIDs(tt.req))
+		})
+	}
 }
